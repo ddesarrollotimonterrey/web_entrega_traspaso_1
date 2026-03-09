94a82a2ed73f9dfd4473af2628d5ffc002ef4ebd
Julieta Flores Tantani <julietaflorestantani@gmail.com>
2026-03-09 10:16:03 -0400

    ccc123

--- servicio.php
+++ servicio.php
@@ -858,7 +858,7 @@ switch ($call) {
                             "Id" => 0,
                             "Lista" => json_encode($datat),
                             "Estado" => 0,
-                            "Mensaje" => "El documento no exite o no es un documento comercial !!!",
+                            "Mensaje" => "El documento no exite o no es un documento comercial !!! ".$datat[0]['ALMACEN_D'].' '.$Almacen,
                         ];
                     }
                 } else {
